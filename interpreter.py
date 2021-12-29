eef20ef2e19dfb4053c8313b8dc94148ab3f0452
ArvidHast <hasta@student.chalmers.se>
2021-12-29 14:18:21 +0100

    Init

---
+++ interpreter.py
@@ -0,0 +1,56 @@
+instructions = ">++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-]<+."
+
+cells = {}
+programPointer = 0
+
+instructionPointer = 0
+
+depth = 0
+enterPoint = {}
+
+while instructionPointer < len(instructions):
+  c = instructions[instructionPointer]
+
+  if c == '>':
+    programPointer += 1
+    if not programPointer in cells:
+      cells[programPointer] = 0
+
+  if c == '<':
+    programPointer -= 1
+    if not programPointer in cells:
+      cells[programPointer] = 0
+
+  if c == '+':
+    cells[programPointer] += 1
+
+  if c == '-':
+    cells[programPointer] -= 1
+
+  if c == '.':
+    print(chr(cells[programPointer]))
+    
+  if c == ',':
+    cells[programPointer] = ord(input())
+
+  if c == '[':
+    if cells[programPointer]:
+      depth += 1
+      enterPoint[depth] = instructionPointer - 1
+    else:
+      print("a", instructionPointer)
+      while c != ']':
+
+        instructionPointer += 1
+        c = instructions[instructionPointer]
+
+
+      print("b", instructionPointer)
+
+  if c == ']':
+    if cells[programPointer]:
+      instructionPointer = enterPoint[depth]
+      depth -= 1
+
+  instructionPointer += 1
+  
